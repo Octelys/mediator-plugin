63ea7847140a194ccc127232ce9cb9eaf629ed8c
Christophe <38137839+kzryzstof@users.noreply.github.com>
2026-08-20 16:12:23 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("java")
     alias(libs.plugins.kotlinJvm)
     id("org.jetbrains.intellij.platform") version "2.18.1"     // See https://github.com/JetBrains/intellij-platform-gradle-plugin/releases
-    id("me.filippov.gradle.jvm.wrapper") version "0.14.0"
+    id("me.filippov.gradle.jvm.wrapper") version "0.16.0"
 }
 
 val isWindows = Os.isFamily(Os.FAMILY_WINDOWS)
@@ -35,7 +35,7 @@ repositories {
 }
 
 tasks.wrapper {
-    gradleVersion = "9.0"
+    gradleVersion = "9.7.1"
     distributionType = Wrapper.DistributionType.ALL
     distributionUrl = "https://cache-redirector.jetbrains.com/services.gradle.org/distributions/gradle-${gradleVersion}-all.zip"
 }
